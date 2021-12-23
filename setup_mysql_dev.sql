ee3dfe8a978b3bb691ebec24f4421d75b825beab
Ignacio Chitnisky <elinmortal@gmail.com>
2021-12-23 16:13:16 +0000

    rewrite sql files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,10 @@
--- prepares a MySQL server
+-- prepares a MySQL server for the project
+-- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS  'hbnb_dev'@'localhost';
+-- A new user hbnb_dev (in localhost)
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+-- hbnb_dev should have all privileges on the database hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- hbnb_dev should have SELECT privilege on the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
