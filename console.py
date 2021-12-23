7eb6cb9a5a851bc9defb2449fc89529753891efc
Ignacio Chitnisky <elinmortal@gmail.com>
2021-12-23 16:24:59 +0000

    fixing console pep8

--- console.py
+++ console.py
@@ -74,7 +74,6 @@ class HBNBCommand(cmd.Cmd):
                 if pline:
                     # check for *args or **kwargs
                     if pline[0] == '{' and pline[-1] == '}'\
-                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
