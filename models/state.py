8a61dc54101034f0907df787cdab8c99b596169f
Ignacio Chitnisky <elinmortal@gmail.com>
2021-12-23 15:50:13 +0000

    adding state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if (getenv("HBNB_TYPE_STORAGE") == "db"):
+        cities = relationship('City', backref="state",
+                              cascade="all, delete-orphan")
+
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            mycities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    mycities.append(city)
+            return mycities
+    
