6fadf8949c20fc829f15d33ce4de052fe3677bde
Ignacio Chitnisky <elinmortal@gmail.com>
2021-12-23 15:15:56 +0000

    adding dbstorage to init

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+rom models.engine.db_storage import DBStorage
+from os import getenv
 
-
-storage = FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
