30781a5fc11e9d859c6361a40696dd3c54d78d27
jopinta <tatinfa@yahoo.com>
2022-01-13 15:47:06 -0800

    fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Fabric script that generates a .tgz archive'''
+
+from fabric.api import run, local
+from datetime import datetime
+
+def do_pack():
+    '''for the tgz'''
+    try:
+        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = "versions/web_static_" + d_t + ".tgz"
+        local ('mkdir -p versions')
+        local ("tar -cvzf" + filename + " web_static")
+        return filename
+    except:
+        return None
