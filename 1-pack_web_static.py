cd4c93e33e47d8d47247bb74daae22eff0d3b36b
jopinta <tatinfa@yahoo.com>
2022-01-13 15:54:01 -0800

    except

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,13 +4,14 @@
 from fabric.api import run, local
 from datetime import datetime
 
+
 def do_pack():
     '''for the tgz'''
     try:
+        local('mkdir -p versions')
         d_t = datetime.now().strftime("%Y%m%d%H%M%S")
         filename = "versions/web_static_" + d_t + ".tgz"
-        local ('mkdir -p versions')
-        local ("tar -cvzf" + filename + " web_static")
+        local("tar -cvzf" + filename + " web_static")
         return filename
     except:
         return None
