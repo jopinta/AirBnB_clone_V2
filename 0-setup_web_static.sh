471f71c01c2cd1beb6bdd5105319b0ea9a86ef55
jopinta <tatinfa@yahoo.com>
2022-01-13 13:16:43 -0800

    symbolic link creared

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#script that sets up your web servers for the deployment
+sudo su
+apt-get update -y
+apt-get install nginx -y
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+printf %s "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/\
+current/
+chown -R ubuntu:ubuntu /data/
+sed -i "/listen 80 default_server/a location /hbnb_stati\
+c/ { alias /data/web_static/current/; autoindex off;}" /\
+etc/nginx/sites-available/default
+service nginx restart
