66abc773a8c7e5c13b26b9ed87744a3fb30e6b44
jopinta <tatinfa@yahoo.com>
2022-01-16 15:48:41 -0800

    no chgrp

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,6 @@ printf %s "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current/
-chown -R ubuntu /data/
-chgrp -R ubuntu /data
+chown -R ubuntu:ubuntu /data/
 sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" \etc/nginx/sites-available/default
 service nginx start
