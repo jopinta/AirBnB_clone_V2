3f0698a9d4bbe72b7a5152acfef458e087ce9d0f
jopinta <tatinfa@yahoo.com>
2022-01-29 18:44:44 -0800

    yting with url map

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 '''starting flasf web application'''
 
 from flask import Flask
 app = Flask(__name__)
+app.url_map.srict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     '''adding perms '''
     return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0.', port=5000, debug=True)
+    app.run(host='0.0.0.0.', port=5000)
