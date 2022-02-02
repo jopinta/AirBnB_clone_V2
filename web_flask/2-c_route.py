4b30523705587b43b3fa14551621cf0199081c9e
jopinta <tatinfa@yahoo.com>
2022-02-02 12:36:59 -0800

    space added

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,9 +18,9 @@ def hi_hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text():
+def c_is_fun(text):
     '''display with value'''
-    return 'C {}'.format(text.replace('_', ''))
+    return ('C {}'.format(text.replace('_', ' ')))
 
 
 if __name__ == "__main__":
