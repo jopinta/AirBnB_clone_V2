68da81256beb33241ff5fabd6a0a09e09a62f70a
jopinta <tatinfa@yahoo.com>
2022-02-02 13:51:45 -0800

    add dislpay

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,33 +9,39 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    '''adding routes'''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hi_hbnb():
+    '''an other one'''
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text():
+    '''display with value'''
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/pyrhon/<text>')
 def python(text="is cool"):
+    '''same with py'''
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
+    '''same with integer'''
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template(n):
-    return render_template('5-number.html', num=n)
+     """display if n is an int"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
