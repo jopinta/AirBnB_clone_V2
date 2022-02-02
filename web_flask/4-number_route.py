1303d1df042f92b47c0f4652586422e5f5ebc7f6
jopinta <tatinfa@yahoo.com>
2022-02-02 13:24:53 -0800

    strict_slash added

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,32 +5,32 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     '''to display'''
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hi_hbnb():
     ''' an other one'''
     return 'HBNB'
 
-@app.route('/c/<text>',strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_text():
     '''dispaly with value'''
     return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python/')
-@app.route('/pyrhon/<text>')
+@app.route('/python/', strict_slashes=false)
+@app.route('/pyyhon/<text>')
 def python(text="is cool"):
     '''same with py'''
     return 'Python {}'.format(text.replace('_', ' '))
 
-@app.route("/number/<int:n>")
+@app.route("/number/<int:n>", strict_slashes=false)
 def number(n):
     '''display int'''
     return '{} is a number'.format(n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0.', port=5000)
+    app.run(host='0.0.0.0', port=5000)
