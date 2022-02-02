dd71f09a6b5cfdb569b9b288dc57543bb4ce38aa
jopinta <tatinfa@yahoo.com>
2022-02-02 13:53:10 -0800

    add dislpay

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template(n):
      """display if n is an int"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', num=n)
 
 
 if __name__ == "__main__":
