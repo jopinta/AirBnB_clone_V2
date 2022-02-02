34607c132a8ffbf9341a0abe77aca505fb893194
jopinta <tatinfa@yahoo.com>
2022-02-02 14:04:41 -0800

    indentation problem

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template(n):
-     """display if n is an int"""
+    """display if n is an int"""
     return render_template('5-number.html', n=n)
 
 
